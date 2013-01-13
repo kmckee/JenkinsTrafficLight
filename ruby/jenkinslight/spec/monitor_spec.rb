feb75a857ece8d379361d18a8661fdc523c47273
Kyle McKee <mckee.kyle@gmail.com>
2013-01-12 20:39:52 -0500

    Refactored spec to clarify intent

--- ruby/jenkinslight/spec/monitor_spec.rb
+++ ruby/jenkinslight/spec/monitor_spec.rb
@@ -3,21 +3,18 @@ require_relative 'spec_helper'
 module JenkinsLight
   describe Monitor do
     
-    it "should output a startup message" do 
-      output = double('output').as_null_object
-      monitor = Monitor.new(output)
-
-      output.should_receive(:puts).with("Jenkins Build Light Monitor Started")
-      
-      monitor.start
+    describe "#new" do
+      let(:output) { double('output').as_null_object }
+      let(:monitor) { Monitor.new(output) }
+      it "should output a startup message" do 
+        output.should_receive(:puts).with("Jenkins Build Light Monitor Started")
+        monitor.start
+      end
+      it "should request the user enter the url of a Jenkins job" do
+        output.should_receive(:puts).with("Enter the URL of the Jenkins job to monitor:")
+        monitor.start
+      end
     end
-    it "should request the user enter the url of a Jenkins job" do
-      output = double('output').as_null_object
-      monitor = Monitor.new(output)
 
-      output.should_receive(:puts).with("Enter the URL of the Jenkins job to monitor:")
-      
-      monitor.start
-    end
   end
 end
