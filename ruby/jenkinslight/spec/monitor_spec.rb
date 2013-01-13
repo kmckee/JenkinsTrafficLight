3e70004af0ac16bec3deb772cdf13238faf8631f
Kyle McKee <mckee.kyle@gmail.com>
2013-01-12 19:47:43 -0500

    Added skipped specs for Monitor class.

---
+++ ruby/jenkinslight/spec/monitor_spec.rb
@@ -0,0 +1,6 @@
+require_relative 'spec_helper'
+
+describe JenkinsLight::Monitor do
+  it "should output a message indicating it has been started"
+  it "should output a message requesting the user enter the url of a job"
+end
