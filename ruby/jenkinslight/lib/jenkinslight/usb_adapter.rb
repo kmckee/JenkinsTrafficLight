500bbdac78ab29652117c8185aff7575b2125ccf
Kyle McKee <mckee.kyle@gmail.com>
2013-01-26 19:49:07 -0500

    Drove out turning on red through the usb adapter

--- ruby/jenkinslight/lib/jenkinslight/usb_adapter.rb
+++ ruby/jenkinslight/lib/jenkinslight/usb_adapter.rb
@@ -1,5 +1,11 @@
 module JenkinsLight
   class UsbAdapter
+    def initialize port
+      @port = port
+    end
 
+    def turn_on color
+      @port.write("1")
+    end
   end
 end
--- ruby/jenkinslight/spec/usb_adapter_spec.rb
+++ ruby/jenkinslight/spec/usb_adapter_spec.rb
@@ -1,5 +1,23 @@
 require 'rspec'
 
-describe JenkinsLight::UsbAdapter do
-
+module JenkinsLight
+  describe UsbAdapter do
+    describe "#turn_on" do
+      it "writes a '1' to the serial port when passed :red" do
+        port = double('port').as_null_object
+        subject = UsbAdapter.new port
+        
+        port.should_receive(:write).with("1")
+        
+        subject.turn_on :red
+      end
+      it "writes a '2' to the serial port when passed :yellow"
+      it "writes a '3' to the serial port when passed :green"
+    end
+    describe "#turn_off" do
+      it "writes a '5' to the serial port when passed :red" 
+      it "writes a '6' to the serial port when passed :yellow"
+      it "writes a '7' to the serial port when passed :green"
+    end
+  end
 end
