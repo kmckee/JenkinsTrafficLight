cf1a8e91f1bb0465345b8c2c36ceb76d9fe21b79
Kyle McKee <mckee.kyle@gmail.com>
2013-02-01 09:12:38 -0500

    For some reason arduino demanded I move this file.  Also added some features so that the light turns off after losing communication for a period of time.

--- Teensy2.0/TeensyLightControllerSketch/TeensyLightControllerSketch.ino
+++ Teensy2.0/TeensyLightControllerSketch/TeensyLightControllerSketch.ino
@@ -1,6 +1,6 @@
-const int redPin = 0;
-const int yellowPin = 1;
-const int greenPin = 2;
+const int redPin = 7;
+const int yellowPin = 6;
+const int greenPin = 5;
 
 const int ON = LOW;
 const int OFF = HIGH;
@@ -22,33 +22,42 @@ void setup() {
 
 
 void loop() {
-  if (Serial.available()) {
-    byte incomingByte = Serial.read();
-    Serial.println(incomingByte);
-    if (incomingByte == 49) // 1 - red, on
+
+    while(Serial.available()) {
+      byte incomingByte = Serial.read();
+      Serial.println(incomingByte);
+      updateLightColor(incomingByte);
+    }
+    
+    delay(1000);
+
+}
+
+void updateLightColor(byte command)
+{
+    if (command == 49) // 1 - red, on
     {
       digitalWrite(redPin, ON);
     }
-    else if (incomingByte == 50) // 2 - yellow, on
+    else if (command == 50) // 2 - yellow, on
     {
       digitalWrite(yellowPin, ON);
     }
-    else if (incomingByte == 51) // 3 - green, on
+    else if (command == 51) // 3 - green, on
     {
       digitalWrite(greenPin, ON);
     }
-    else if (incomingByte == 53) // 5 - red, off
+    else if (command == 53) // 5 - red, off
     {
       digitalWrite(redPin, OFF);
     }
-    else if (incomingByte == 54) // 6 - yellow, off
+    else if (command == 54) // 6 - yellow, off
     {
       digitalWrite(yellowPin, OFF);
     }
-    else if (incomingByte == 55) // 7 - green, on
+    else if (command == 55) // 7 - green, on
     {
       digitalWrite(greenPin, OFF);
     }
-  }
 }
 
